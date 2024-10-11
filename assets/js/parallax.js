20925610bd6b4ed398921ddf4eecc7cde10d85b3
Jamshid <jamshid_tojiboyev@mail.ru>
2024-10-11 20:37:41 +0500

    added

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,5 +1,5 @@
 const parallax = document.querySelector('.parallax');
-const objectImgs = document.querySelectorAll('img.object');
+const objectImgs = document.querySelectorAll('.parallax .object');
 
 parallax.addEventListener('mousemove', (e) => {
     objectImgs.forEach(img => {
@@ -9,4 +9,17 @@ parallax.addEventListener('mousemove', (e) => {
 
         img.style.transform = "translateX(" + x + "px) translateY(" + y + "px)"
     })
-})
+});
+
+const prof_parallax = document.querySelector('.prof-parallax');
+const prof_objectImgs = document.querySelectorAll('.prof-parallax .object');
+
+prof_parallax.addEventListener('mousemove', (e) => {
+    prof_objectImgs.forEach(img => {
+        let moving_value = img.getAttribute("data-value");
+        let x = (e.clientX * moving_value) / 200;
+        let y = (e.clientY * moving_value) / 200;
+
+        img.style.transform = "translateX(" + x + "px) translateY(" + y + "px)"
+    })
+});
