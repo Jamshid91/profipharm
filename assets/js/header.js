7cbc9821daa69f4ce2550f79a189231244f68747
Jamshid <jamshid_tojiboyev@mail.ru>
2024-10-13 14:09:12 +0500

    added

--- assets/js/header.js
+++ assets/js/header.js
@@ -22,22 +22,26 @@ menuBtn.addEventListener('click', () => {
     body.classList.toggle('bodyStopScroll');
     menu_box.classList.remove('d-none');
     projects.classList.add('d-none');
+    menu_close.classList.remove('d-none');
 });
 
 projectsShow.addEventListener('click', () => {
     menu_box.classList.add('d-none');
     projects.classList.remove('d-none');
+    menu_close.classList.add('d-none');
 });
 
 backToMenu.addEventListener('click', () => {
     menu_box.classList.remove('d-none');
     projects.classList.add('d-none');
+    menu_close.classList.remove('d-none');
 });
 
 menu_close.addEventListener('click', () => {
     project_btn.classList.remove('clickPr-btn');
     menu.classList.add('d-none');
     menuBtn.classList.remove('clickBtn');
+    menu_close.classList.remove('d-none');
     body.classList.remove('bodyStopScroll');
 });
 
